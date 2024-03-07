980cb84c8c48068c93f9153b0d2d009bdeb1bb22
shirin faile <shirin.faile@hotmail.com>
2024-03-07 10:44:56 +0100

    added subclass subpage

--- routes/roles.js
+++ routes/roles.js
@@ -1,9 +1,14 @@
 import express from 'express';
 import { roles } from '../data/roles-info.js';
+import { subClasses } from '../data/subclass-info.js';
 const rolesRouter = express.Router();
 
 rolesRouter.get ('/', (req, res) => {
     res.render('pages/roles', {roles: roles});
-})
+});
+
+rolesRouter.get('/subclasses', (req, res) => {
+    res.render('pages/subclasses', {subClasses: subClasses});
+});
 
 export default rolesRouter;
